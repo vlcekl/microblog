182790445efa98f7ce8f67a11b262770d95687e4
Lukas Vlcek <lukas_vlcek@yahoo.com>
2020-10-23 08:00:20 -0500

    added _post subtemplate

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField,
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms import TextAreaField 
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from wtforms.validators import Length
